3444722017ca0ccf610171a82f30ac6b12ab127b
monkeysoop <istok.endre2@gmail.com>
2026-04-29 13:44:49 +0200

    halve the computation cost by using rfft (our inputs are real valued thus the normal fft has Hermitian symmetry), also made audio strength calculation independent of the sample window size

--- beamforming.py
+++ beamforming.py
@@ -40,18 +40,20 @@ relative_microphone_positions = jax.device_put(MICROPHONE_POSITIONS - CAMERA_POS
 camera_pixel_microphone_time_offsets = jax.numpy.tensordot(camera_directions, relative_microphone_positions, axes=([2], [1])) / SPEED_OF_SOUND
 
 @jax.jit
-def compute_audio_strengths_for_a_chunk(camera_pixel_microphone_time_offsets, data_fft, data_frequencies):
+def compute_audio_strengths_for_a_chunk(camera_pixel_microphone_time_offsets, data_fft, data_frequencies, n):
     number_of_microphones = camera_pixel_microphone_time_offsets.shape[1]
 
     data_shift = jax.numpy.exp(-2.0j * jax.numpy.pi * camera_pixel_microphone_time_offsets[:, jax.numpy.newaxis, :] * data_frequencies[jax.numpy.newaxis, :, jax.numpy.newaxis])
     data_fft_shifted_summed = jax.numpy.einsum("sm,psm->ps", data_fft, data_shift)
-    audio_strengths = jax.numpy.mean(jax.numpy.square(jax.numpy.abs(data_fft_shifted_summed)), axis=1) / number_of_microphones
+    audio_powers = jax.numpy.square(jax.numpy.abs(data_fft_shifted_summed))
+    audio_powers = audio_power.at[:, 1:-1].multiply(2.0)
+    audio_strengths = jax.numpy.sqrt(jax.numpy.sum(audio_powers, axis=1)) / n
 
     return audio_strengths
 
 def compute_audio_strengths(microphone_samples, n, microphone_sample_rate, camera_pixel_microphone_time_offsets, pixel_chunk_size):
-    data_fft = jax.numpy.fft.fft(microphone_samples, axis=0)
-    data_frequencies = jax.numpy.fft.fftfreq(n, d=(1.0 / microphone_sample_rate))
+    data_fft = jax.numpy.fft.rfft(microphone_samples, axis=0)
+    data_frequencies = jax.numpy.fft.rfftfreq(n, d=(1.0 / microphone_sample_rate))
 
     original_shape = camera_pixel_microphone_time_offsets.shape
     number_of_pixels = original_shape[0] * original_shape[1]
@@ -64,11 +66,11 @@ def compute_audio_strengths(microphone_samples, n, microphone_sample_rate, camer
 
     audio_strengths = jax.device_put(jax.numpy.zeros((number_of_pixels)))
     for delimiter, size in zip(delimiters, sizes):
-        audio_strengths = audio_strengths.at[delimiter:(delimiter + size)].set(compute_audio_strengths_for_a_chunk(camera_pixel_microphone_time_offsets[delimiter:(delimiter + size), :], data_fft, data_frequencies))
+        audio_strengths = audio_strengths.at[delimiter:(delimiter + size)].set(compute_audio_strengths_for_a_chunk(camera_pixel_microphone_time_offsets[delimiter:(delimiter + size), :], data_fft, data_frequencies, n))
 
     audio_strengths = jax.numpy.reshape(audio_strengths, (original_shape[0], original_shape[1]))
 
-    return audio_strengths
+    return 20.0 * jax.numpy.log10(audio_strengths + 1e-12)
 
 N = 1024
 
